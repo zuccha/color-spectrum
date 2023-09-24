4eebd235bef2a27434964552afc5b5647df9aaa7
Amedeo Zucchetti <amedeo.zucchetti@gmail.com>
2023-09-24 16:32:13 +0200

    add sound effect when collecting brush

--- Assets/Scripts/Items/BrushItem.cs
+++ Assets/Scripts/Items/BrushItem.cs
@@ -4,12 +4,17 @@ using UnityEngine;
 
 public class BrushItem : MonoBehaviour
 {
+    [Header("Audio")]
+    [SerializeField]
+    private AudioClip _collectSplashClip;
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         Player player = other.GetComponent<Player>();
         if (player)
         {
             player.SetHasBrush(true);
+            AudioManager.Instance.PlayEffect(_collectSplashClip, 1.1f);
             gameObject.SetActive(false);
         }
     }
