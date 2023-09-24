126c55889998d8c6b4b4548856333add418b2f16
Amedeo Zucchetti <amedeo.zucchetti@gmail.com>
2023-09-24 03:39:54 +0200

    make healthbar yellow and red on different thresholds

--- Assets/Scripts/UI/Healthbar.cs
+++ Assets/Scripts/UI/Healthbar.cs
@@ -8,8 +8,34 @@ public class Healthbar : MonoBehaviour
     [SerializeField]
     private Image _healthBarSprite;
 
+    [SerializeField]
+    private Sprite _healthBarGreenSprite;
+
+    [SerializeField]
+    private Sprite _healthBarRedSprite;
+
+    [SerializeField]
+    private Sprite _healthBarYellowSprite;
+
     public void UpdateHealthBar(float maxHealth, float currentHealth)
     {
-        _healthBarSprite.fillAmount = currentHealth / maxHealth;
+        float healthProgress = currentHealth / maxHealth;
+        _healthBarSprite.fillAmount = healthProgress;
+
+        if (healthProgress > 0.5)
+        {
+            _healthBarSprite.sprite = _healthBarGreenSprite;
+            _healthBarSprite.color = Color.green;
+        }
+        else if (healthProgress > 0.25)
+        {
+            _healthBarSprite.sprite = _healthBarYellowSprite;
+            _healthBarSprite.color = Color.yellow;
+        }
+        else
+        {
+            _healthBarSprite.sprite = _healthBarRedSprite;
+            _healthBarSprite.color = Color.red;
+        }
     }
 }
